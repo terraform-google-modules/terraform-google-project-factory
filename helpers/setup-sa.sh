3df0f6e3e08415371d68e30e1cd8fb7d3f3968b9
johnrevans6 <johnre@hawaii.edu>
2018-07-29 16:22:17 -0700

    Add support for creating service account (#6)
    * Renamed setup script to setup-sa.sh
    
    * Added service account creation to setup.sh
    
    * Suppressed standard output in setup-sa.sh
    
    * Added user feedback for setup-sa.sh
    
    * Delete credentials.json

--- helpers/setup-sa.sh
+++ helpers/setup-sa.sh
@@ -17,52 +17,87 @@
 set -e
 
 # Organization ID
-ORG_ID=$1
+ORG_ID="$(gcloud organizations list --format="value(ID)" --filter="$1")"
+
+if [[ $ORG_ID == "" ]];
+then
+  echo "The organization id provided does not exist. Exiting."
+  exit 1;
+fi
 
 # Host project
-HOST_PROJECT=$2
+HOST_PROJECT="$(gcloud projects list --format="value(projectId)" --filter="$2")"
 
-# Service Account ID
-SA_ID=$3
+if [[ $HOST_PROJECT == "" ]];
+then
+  echo "The host project does not exist. Exiting."
+  exit 1;
+fi
 
+# Service Account creation
+SA_NAME="project-factory-${RANDOM}"
+SA_ID="${SA_NAME}@${HOST_PROJECT}.iam.gserviceaccount.com"
+STAGING_DIR="${PWD}"
+KEY_FILE="${STAGING_DIR}/credentials.json"
+
+gcloud iam service-accounts \
+    --project ${HOST_PROJECT} create ${SA_NAME} \
+    --display-name ${SA_NAME}
+
+echo "Downloading key to credentials.json..."
+
+gcloud iam service-accounts keys create ${KEY_FILE} \
+    --iam-account ${SA_ID} \
+    --user-output-enabled false
+
+echo "Applying permissions for org $ORG_ID and project $HOST_PROJECT..."
 # Grant roles/resourcemanager.organizationViewer to the service account on the organization
 gcloud organizations add-iam-policy-binding \
   "${ORG_ID}" \
   --member="serviceAccount:${SA_ID}" \
-  --role="roles/resourcemanager.organizationViewer"
+  --role="roles/resourcemanager.organizationViewer" \
+  --user-output-enabled false
 
 # Grant roles/resourcemanager.projectCreator to the service account on the organization
 gcloud organizations add-iam-policy-binding \
   "${ORG_ID}" \
   --member="serviceAccount:${SA_ID}" \
-  --role="roles/resourcemanager.projectCreator"
+  --role="roles/resourcemanager.projectCreator" \
+  --user-output-enabled false
 
 # Grant roles/billing.user to the service account on the organization
 gcloud organizations add-iam-policy-binding \
   "${ORG_ID}" \
   --member="serviceAccount:${SA_ID}" \
-  --role="roles/billing.user"
+  --role="roles/billing.user" \
+  --user-output-enabled false
 
 # Grant roles/compute.xpnAdmin to the service account on the organization
 gcloud organizations add-iam-policy-binding \
   "${ORG_ID}" \
   --member="serviceAccount:${SA_ID}" \
-  --role="roles/compute.xpnAdmin"
+  --role="roles/compute.xpnAdmin" \
+  --user-output-enabled false
 
 # Grant roles/compute.networkAdmin to the service account on the organization
 gcloud organizations add-iam-policy-binding \
   "${ORG_ID}" \
   --member="serviceAccount:${SA_ID}" \
-  --role="roles/compute.networkAdmin"
+  --role="roles/compute.networkAdmin" \
+  --user-output-enabled false
 
 # Grant roles/iam.serviceAccountAdmin to the service account on the organization
 gcloud organizations add-iam-policy-binding \
   "${ORG_ID}" \
   --member="serviceAccount:${SA_ID}" \
-  --role="roles/iam.serviceAccountAdmin"
+  --role="roles/iam.serviceAccountAdmin" \
+  --user-output-enabled false
 
 # Grant roles/resourcemanager.projectIamAdmin to the service account on the host project
 gcloud projects add-iam-policy-binding \
   "${HOST_PROJECT}" \
   --member="serviceAccount:${SA_ID}" \
-  --role="roles/resourcemanager.projectIamAdmin"
+  --role="roles/resourcemanager.projectIamAdmin" \
+  --user-output-enabled false
+
+echo "All done."
