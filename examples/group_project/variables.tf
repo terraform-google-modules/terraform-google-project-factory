488c9e78cfcb38cadcb58380156e6f202bb7a8a8
Casey Pearring <cpearring@cpearring-macbookpro2.roam.corp.google.com>
2018-09-19 08:30:10 -0700

    removing set variables

--- examples/group_project/variables.tf
+++ examples/group_project/variables.tf
@@ -16,30 +16,24 @@
 
 variable "admin_email" {
   description = "Admin user email on Gsuite"
-  default     = "casey@phoogle.net"
 }
 
 variable "credentials_file_path" {
   description = "Service account json auth path"
-  default     = "~/testing/serviceAccountKeys/orgserviceaccount.json"
 }
 
 variable "organization_id" {
   description = "The organization id for the associated services"
-  default     = "826592752744"
 }
 
 variable "billing_account" {
   description = "The ID of the billing account to associate this project with"
-  default     = "01E8A0-35F760-5CF02A"
 }
 
 variable "api_sa_group" {
   description = "An existing GSuite group email to place the Google APIs Service Account for the project in"
-  default     = "api_sa_group@phoogle.net"
 }
 
 variable "project_group_name" {
   description = "The name of a GSuite group to create for controlling the project"
-  default     = "cpearring-test"
 }
