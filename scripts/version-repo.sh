0289203f7a8fc1e65608e09aa6ca16d482110c95
Jonathan Mickle <mickle@google.com>
2018-09-21 11:16:07 -0700

    removing the sort

--- scripts/version-repo.sh
+++ scripts/version-repo.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-REPO_VER=v`cat ../CHANGELOG.md | grep "##" | sort -r | head -n 1 | cut -d" " -f2`
+REPO_VER=v`cat ../CHANGELOG.md | grep "##" | head -n 1 | cut -d" " -f2`
 CUR_GIT_TAG=`git describe --abbrev=0 --tags`
 INGIT=false
 
