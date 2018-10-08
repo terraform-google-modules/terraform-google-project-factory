dc76bc2def170948ff4d8005b78a79a92ed5353c
Eric Johnson <erjohnso@google.com>
2018-10-08 10:55:53 -0700

    Fix parsing error for Terraform public registry.

--- variables.tf
+++ variables.tf
@@ -48,7 +48,7 @@ variable "folder_id" {
 }
 
 variable "group_name" {
-  description = "A group to control the project by being assigned group_role - defaults to ${project_name}-editors"
+  description = "A group to control the project by being assigned group_role (defaults to project editor)"
   default     = ""
 }
 
