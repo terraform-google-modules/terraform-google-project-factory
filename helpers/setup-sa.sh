544e4eead32695c1393d597c2831223c42f23830
Brandon Knitter <knitterb@blandsite.org>
2018-10-16 11:08:59 -0700

    Add usage information to helper script (#51)
    * add a check for the variables to be passed and if missing print the usage
    
    * removing newline at end of file

--- helpers/setup-sa.sh
+++ helpers/setup-sa.sh
@@ -17,6 +17,14 @@
 set -e
 set -u
 
+# check for input variables
+if [ $# -ne 2 ]; then
+  echo
+  echo "Usage: $0 <organization name> <project id>"
+  echo
+  exit 1
+fi
+
 # Organization ID
 ORG_ID="$(gcloud organizations list --format="value(ID)" --filter="$1")"
 
