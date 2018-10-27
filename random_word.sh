af14c22b9321889fb979af723d8d71a1d64b617a
johnnydrama92 <james.ferrier01@gmail.com>
2018-10-28 00:55:40 +0100

    removed bash and dict

--- random_word.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-MYDIR="$(dirname "$(readlink -f "$0")")"
-word1="$(shuf -n1 "${MYDIR}"/dictionary.txt)"
-word2="$(shuf -n1 "${MYDIR}"/dictionary.txt)"
-
-jq -n --arg word1 "${word1}" --arg word2 "${word2}" '{"word1":$word1,"word2":$word2}'
