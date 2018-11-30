5033da8e93a0d70363fef0603924858dc88ba9bb
Aaron Lane <aarondrewlane@gmail.com>
2018-11-30 17:35:45 +0000

    Pass full var.domain to gsuite_enabled

--- test/fixtures/full/main.tf
+++ test/fixtures/full/main.tf
@@ -16,7 +16,7 @@
 
 provider "google" {
   credentials = "${file(var.credentials_path)}"
-  version = "~> 1.19"
+  version     = "~> 1.19"
 }
 
 provider "gsuite" {
@@ -57,7 +57,9 @@ module "vpc" {
 }
 
 module "project-factory" {
-  source              = "../../../modules/gsuite_enabled"
+  source = "../../../modules/gsuite_enabled"
+
+  domain              = "${var.domain}"
   name                = "${var.name}"
   random_project_id   = true
   org_id              = "${var.org_id}"
