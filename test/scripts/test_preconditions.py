b222ff029f3522153a0242ceaa1da64295725ccb
Aaron Lane <aarondrewlane@gmail.com>
2018-12-19 16:21:06 +0000

    Correct names and paths of preconditions files
    This commit corrects the filename of the preconditions module, the
    path of the test_preconditions module, and the reference to
    preconditions from test_preconditions.

--- test/scripts/test_preconditions.py
+++ test/scripts/test_preconditions.py
@@ -21,7 +21,7 @@ sys.path.append(
     os.path.abspath(
         os.path.join(
             os.path.dirname(__file__),
-            '../../helpers/')))
+            '../../scripts/')))
 
 import preconditions  # noqa: E402
 
