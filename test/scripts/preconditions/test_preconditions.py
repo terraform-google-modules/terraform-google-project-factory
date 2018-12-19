48c4c8360fc87dc943d24a4022a8baf2849313e4
Aaron Lane <aarondrewlane@gmail.com>
2018-12-19 17:46:22 +0000

    Fix import of preconditions module

--- test/scripts/preconditions/test_preconditions.py
+++ test/scripts/preconditions/test_preconditions.py
@@ -21,7 +21,7 @@ sys.path.append(
     os.path.abspath(
         os.path.join(
             os.path.dirname(__file__),
-            '../../../scripts')))
+            '../../../scripts/preconditions')))
 
 import preconditions  # noqa: E402
 
