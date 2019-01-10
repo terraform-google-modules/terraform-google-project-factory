be7aa85019a1a6dc89f44d23e11921ef6fa62e39
Jason Berlinsky <jason@barefootcoders.com>
2019-01-10 16:08:49 -0500

    Ignore .terraform when running validation

--- test/make.sh
+++ test/make.sh
@@ -48,7 +48,7 @@ function docker() {
 function check_terraform() {
   echo "Running terraform validate"
   #shellcheck disable=SC2156
-  find . -name "*.tf" -not -path "./test/fixtures/shared/*" -exec bash -c 'terraform validate --check-variables=false $(dirname "{}")' \;
+  find . -name "*.tf" -not -path "./**/.terraform/*" -not -path "./test/fixtures/shared/*" -exec bash -c 'terraform validate --check-variables=false $(dirname "{}")' \;
 }
 
 # This function runs 'go fmt' and 'go vet' on eery file
