3a9c21389fc2f827f8cfd9a9ba6ec98e5c95be2d
Danny Seymour <dseymour@taos.com>
2019-03-06 16:35:08 -0800

    Update README and examples to reflect the actual supported provider versions

--- examples/gke_shared_vpc/main.tf
+++ examples/gke_shared_vpc/main.tf
@@ -20,12 +20,12 @@ locals {
 
 provider "google" {
   credentials = "${file(local.credentials_file_path)}"
-  version     = "~> 1.19"
+  version     = "~> 2.1"
 }
 
 provider "google-beta" {
   credentials = "${file(local.credentials_file_path)}"
-  version     = "~> 1.19"
+  version     = "~> 2.1"
 }
 
 module "project-factory" {
