8f066b8ac2828a86ab1cf0ca2a2a0566eddb8961
Bruno Amaral <brunoamaralf@gmail.com>
2019-04-07 10:36:50 -0300

    Add terraform providers.

---
+++ providers.tf
@@ -0,0 +1,19 @@
+data "google_compute_zones" "available" {
+    region = "${var.region}"
+}
+
+
+provider "google" {
+//  credentials = "${file("~/.config/gcloud/terraform-g5search.json")}"
+  region      = "${var.region}"
+  zone        = "${data.google_compute_zones.available.names[0]}"
+}
+
+provider "google-beta" {
+//  credentials = "${file("~/.config/gcloud/terraform-g5search.json")}"
+  region      = "${var.region}"
+  zone        = "${data.google_compute_zones.available.names[0]}"
+}
+
+
+
