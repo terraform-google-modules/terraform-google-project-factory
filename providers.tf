3c84fd975a982420a3d1ef56625467938e092263
Bruno Amaral <brunoamaralf@gmail.com>
2019-04-07 10:56:35 -0300

    Update providers config.

--- providers.tf
+++ providers.tf
@@ -2,21 +2,13 @@ variable "region" {
   description = "The region to be set on the providers."
 }
 
-data "google_compute_zones" "available" {
-    region = "${var.region}"
-}
-
 
 provider "google" {
-//  credentials = "${file("~/.config/gcloud/terraform-g5search.json")}"
   region      = "${var.region}"
-  zone        = "${data.google_compute_zones.available.names[0]}"
 }
 
 provider "google-beta" {
-//  credentials = "${file("~/.config/gcloud/terraform-g5search.json")}"
   region      = "${var.region}"
-  zone        = "${data.google_compute_zones.available.names[0]}"
 }
 
 
