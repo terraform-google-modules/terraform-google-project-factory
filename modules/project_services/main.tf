8e780a02fc1e29303ea2f32dd04b1cc25708ebec
Alex Konkin <alexkonkin@gmail.com>
2019-05-02 22:29:20 +0300

    fixed typo in the name of variable

--- modules/project_services/main.tf
+++ modules/project_services/main.tf
@@ -22,5 +22,5 @@ resource "google_project_service" "project_services" {
   project            = "${var.project_id}"
   service            = "${element(var.activate_apis, count.index)}"
   disable_on_destroy = "${var.disable_services_on_destroy}"
-  disable_dependent_services = "${var.disable_dependant_services}"
+  disable_dependent_services = "${var.disable_dependent_services}"
 }
