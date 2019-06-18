129acf02096f944e645f85fbf52863a3ee6c445b
Ihor Pukha <sorceress1979@gmail.com>
2019-06-18 15:28:34 +0300

    5-th example fixed plus all modules

--- modules/gsuite_group/main.tf
+++ modules/gsuite_group/main.tf
@@ -15,15 +15,16 @@
  */
 
 locals {
-  domain_list = "${concat(data.google_organization.org.*.domain, list("dummy"))}"
-  domain      = "${var.domain == "" ? element(local.domain_list, 0) : var.domain}"
-  email       = "${var.name == "" ? "" : format("%s@%s", var.name, local.domain)}"
+  domain_list = concat(data.google_organization.org.*.domain, ["dummy"])
+  domain      = var.domain == "" ? element(local.domain_list, 0) : var.domain
+  email       = var.name == "" ? "" : format("%s@%s", var.name, local.domain)
 }
 
 /*****************************************
   Organization info retrieval
  *****************************************/
 data "google_organization" "org" {
-  count        = "${var.domain == "" ? 1 : 0}"
-  organization = "${var.org_id}"
+  count        = var.domain == "" ? 1 : 0
+  organization = var.org_id
 }
+
