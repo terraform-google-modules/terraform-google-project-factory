9d24a636deac5b12f98dc2ef248c8a2dd9e16762
Thiago Carvalho <thiagonbcarvalho@gmail.com>
2020-09-22 22:30:04 -0300

    Updates module name in the outputs

--- test/fixtures/svpc_host_project/outputs.tf
+++ test/fixtures/svpc_host_project/outputs.tf
@@ -15,64 +15,64 @@
  */
 
 output "service_project_name" {
-  value       = module.example.service_project.project_name
+  value       = module.host_project.service_project.project_name
   description = "The service project name"
 }
 
 output "service_project_id" {
-  value       = module.example.service_project.project_id
+  value       = module.host_project.service_project.project_id
   description = "The service project ID"
 }
 
 output "service_project_ids" {
   value = [
-    module.example.service_project.project_id,
-    module.example.service_project_b.project_id
+    module.host_project.service_project.project_id,
+    module.host_project.service_project_b.project_id
   ]
   description = "The service project IDs"
 }
 
 output "service_project_number" {
-  value       = module.example.service_project.project_number
+  value       = module.host_project.service_project.project_number
   description = "The service project number"
 }
 
 output "service_project_b_number" {
-  value       = module.example.service_project_b.project_number
+  value       = module.host_project.service_project_b.project_number
   description = "The service project b number"
 }
 
 output "service_account_email" {
-  value       = module.example.service_project.service_account_email
+  value       = module.host_project.service_project.service_account_email
   description = "The service account email"
 }
 
 output "secondary_service_account_email" {
-  value       = module.example.service_project_b.service_account_email
+  value       = module.host_project.service_project_b.service_account_email
   description = "The secondary service account email"
 }
 
 output "shared_vpc" {
-  value       = module.example.host_project.project_id
+  value       = module.host_project.host_project.project_id
   description = "The host project ID"
 }
 
 output "shared_vpc_subnet_name_01" {
-  value       = module.example.vpc.subnets_names[0]
+  value       = module.host_project.vpc.subnets_names[0]
   description = "The first subnet name"
 }
 
 output "shared_vpc_subnet_region_01" {
-  value       = module.example.vpc.subnets_regions[0]
+  value       = module.host_project.vpc.subnets_regions[0]
   description = "The first subnet region"
 }
 
 output "shared_vpc_subnet_name_02" {
-  value       = module.example.vpc.subnets_names[1]
+  value       = module.host_project.vpc.subnets_names[1]
   description = "The second subnet name"
 }
 
 output "shared_vpc_subnet_region_02" {
-  value       = module.example.vpc.subnets_regions[1]
+  value       = module.host_project.vpc.subnets_regions[1]
   description = "The second subnet region"
 }
