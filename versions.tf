921ac9852bfc7595657ab0e1d85a6206ff1d037f
Abhisek Purwar <abhisekpurwar@google.com>
2022-08-16 18:02:00 +0000

    Add support for custom time periods in budget module

--- versions.tf
+++ versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.11"
+      version = "~> 4.28"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.11"
+      version = "~> 4.28"
     }
   }
   provider_meta "google" {
