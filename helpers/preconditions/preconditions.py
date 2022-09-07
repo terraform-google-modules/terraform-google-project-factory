e59732d8d0b72f49fd78b9be897c35440e22bb68
khavert <khavert@gmail.com>
2022-09-07 20:53:04 +0200

    resolve #744 Add possibility to log in with access token.

--- helpers/preconditions/preconditions.py
+++ helpers/preconditions/preconditions.py
@@ -29,11 +29,13 @@ import logging
 import re
 import sys
 import os
+import requests
 
 try:
     import google.auth
     from google.auth import impersonated_credentials
     from google.oauth2 import service_account
+    from google.oauth2 import credentials as oauth_access_token
     from googleapiclient import discovery
 except ImportError as e:
     if os.environ.get('GRACEFUL_IMPORTERROR', '') != '':
@@ -350,9 +352,24 @@ def get_credentials(credentials_path, impersonate_service_account):
     """
     if credentials_path is not None:
         # Prefer an explicit credentials file
-        svc_credentials = service_account.Credentials\
+        svc_credentials = service_account.Credentials \
             .from_service_account_file(credentials_path)
         credentials = (svc_credentials, svc_credentials.project_id)
+    elif os.environ.get('GOOGLE_OAUTH_ACCESS_TOKEN') is not None:
+        oauth_token = os.environ.get('GOOGLE_OAUTH_ACCESS_TOKEN')
+        token_info = requests.post(
+            'https://www.googleapis.com/oauth2/v3/tokeninfo',
+            data=f'access_token={oauth_token}',
+            headers={"Content-Type": "application/x-www-form-urlencoded"}
+        ).json()
+        sa_info_url = f'https://iam.googleapis.com/v1/projects/-/' \
+                      f'serviceAccounts/{token_info["aud"]}'
+        account_info = requests.get(
+            sa_info_url,
+            headers={"Authorization": f"Bearer {oauth_token}"}
+        ).json()
+        credentials = (oauth_access_token.Credentials(oauth_token),
+                       account_info['projectId'])
     elif impersonate_service_account is not None:
         try:
             source_credentials = google.auth.default()
