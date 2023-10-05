6639562d5b8c9fa2cb51ec6bf33abe3a2ccbde4a
Andrew Peabody <andrewpeabody@google.com>
2023-10-05 21:55:18 +0000

    remove other example providers

--- examples/group_project/versions.tf
+++ examples/group_project/versions.tf
@@ -15,25 +15,10 @@
  */
 
 terraform {
-  required_version = ">= 0.13"
   required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-    google-beta = {
-      source = "hashicorp/google-beta"
-    }
     gsuite = {
       source  = "deviavir/gsuite"
       version = "~> 0.1.12"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
     }
   }
 }
--- versions.tf
+++ versions.tf
@@ -18,10 +18,12 @@ terraform {
   required_version = ">=0.13.0"
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
+      version = ">= 4.28, < 6"
     }
     google-beta = {
-      source = "hashicorp/google-beta"
+      source  = "hashicorp/google-beta"
+      version = ">= 4.28, < 6"
     }
   }
   provider_meta "google" {
